a06c153200bd62698b0deed576a3012a027a3123
davidly <davidly_lee@hotmail.com>
2022-06-21 09:22:32 -0700

    add python implementation

---
+++ ttt.py
@@ -0,0 +1,155 @@
+# prove you can't win at tic-tac-tie
+#
+# boards positions:
+#   0  1  2
+#   3  4  5
+#   6  7  8
+#
+
+import array as arr
+import time
+import threading
+import multiprocessing
+
+from multiprocessing.pool import ThreadPool as Pool
+
+evaluated = 0
+iterations = 1000 # not 10k like other implementations because Python is too slow
+piece_blank = 0
+piece_x = 1
+piece_o = 2
+
+score_win = 6
+score_tie = 5
+score_lose = 4
+score_max = 9
+score_min = 2
+
+def look_for_winner( board ):
+    p = board[ 0 ]
+    if piece_blank != p:
+        if p == board[1] and p == board[2]:
+            return p
+        if p == board[3] and p == board[6]:
+            return p
+    p = board[3]
+    if piece_blank != p and p == board[4] and p == board[5]:
+        return p
+    p = board[6]
+    if piece_blank != p and p == board[7] and p == board[8]:
+        return p
+    p = board[1]
+    if piece_blank != p and p == board[4] and p == board[7]:
+        return p
+    p = board[2]
+    if piece_blank != p and p == board[5] and p == board[8]:
+        return p
+    p = board[4]
+    if piece_blank != p:
+        if p == board[0] and p == board[8]:
+            return p
+        if p == board[2] and p == board[6]:
+            return p
+    return piece_blank
+
+def min_max( board, alpha, beta, depth, move ):
+    global evaluated
+    evaluated += 1
+
+    if depth >= 4:
+        p = look_for_winner( board )
+
+        if piece_x == p:
+            return score_win
+
+        if piece_o == p:
+            return score_lose
+
+        if 8 == depth:
+            return score_tie
+
+    value = score_max
+    pieceMove = piece_o
+
+    if 0 != ( depth & 1 ):
+        value = score_min
+        pieceMove = piece_x
+
+    for x in range( 0, 9 ):
+        if piece_blank == board[ x ]:
+            board[ x ] = pieceMove
+            score = min_max( board, alpha, beta, depth + 1, x )
+            board[ x ] = piece_blank
+
+            if 0 != ( depth & 1 ):
+                value = max( value, score )
+                alpha = max( alpha, value )
+                if alpha >= beta:
+                    return value
+                if score_win == value:
+                    return value
+            else:
+                value = min( value, score )
+                beta = min( value, beta )
+                if beta <= alpha:
+                    return value
+                if score_lose == value:
+                    return score_lose
+    return value
+
+def run_board( move ):
+    b = arr.array( 'i', [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ] )
+    b[ move ] = piece_x
+    #print( f"run_board for " + str( move ) )
+
+    for i in range( 0, iterations ):
+        score = min_max( b, score_min, score_max, 0, move )
+        #if score_tie != score:
+        #    print( f"didn't get a tie score!")
+
+serial_start_time = time.time()
+
+run_board( 0 )
+run_board( 1 )
+run_board( 4 )
+
+serial_end_time = time.time()
+serial_time = serial_end_time - serial_start_time
+serial_evaluated = evaluated
+
+evaluated = 0
+parallel_start_time = time.time()
+
+# using multipule threads doesn't help because they all share 1 core
+#pool = Pool()
+#pool.apply_async( run_board, ( 0, ) )
+#pool.apply_async( run_board, ( 1, ) )
+#pool.apply_async( run_board, ( 4, ) )
+#pool.close()
+#pool.join()
+
+p0 = multiprocessing.Process( target=run_board, args=(0,))
+p1 = multiprocessing.Process( target=run_board, args=(1,))
+p4 = multiprocessing.Process( target=run_board, args=(4,))
+
+p0.start()
+p1.start()
+p4.start()
+
+p0.join()
+p1.join()
+p4.join()
+
+parallel_end_time = time.time()
+parallel_time = parallel_end_time - parallel_start_time
+
+# this will be 0 when using multiprocessing because the variable isn't marshalled back
+parallel_evaluated = evaluated
+
+print( f"serial moves evaluated: " + str( serial_evaluated) )
+print( f"    elapsed time: " + str( serial_time ) )
+print( f"    one iteration: " + str( ( serial_time ) / iterations ) )
+print( f"parallel moves evaluated: " + str( parallel_evaluated) )
+print( f"    elapsed time: " + str( parallel_time ) )
+print( f"    one iteration: " + str( ( parallel_time ) / iterations ) )
+
