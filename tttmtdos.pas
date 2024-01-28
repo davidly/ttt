64b6af97c5bbe10b5132ecd184ddebc74b5e99d3
davidly <davidly_lee@hotmail.com>
2024-01-28 08:19:16 -0800

    performance

--- tttmtdos.pas
+++ tttmtdos.pas
@@ -91,7 +91,77 @@ begin
   lookForWinner := p;
 end;
 
-function minmax( alpha: integer; beta: integer; depth: integer ): integer;
+function winner2( move: integer ) : integer;
+var
+    x : integer;
+begin
+    case move of
+        0:  begin
+            x := board[ 0 ];
+            if not ( ( ( x = board[1] ) and ( x = board[2] ) ) or
+                     ( ( x = board[3] ) and ( x = board[6] ) ) or
+                     ( ( x = board[4] ) and ( x = board[8] ) ) )
+                then x := PieceBlank;
+            end;
+        1:  begin
+            x := board[ 1 ];
+            if not ( ( ( x = board[0] ) and ( x = board[2] ) ) or
+                     ( ( x = board[4] ) and ( x = board[7] ) ) )
+                then x := PieceBlank;
+            end;
+        2:  begin
+            x := board[ 2 ];
+            if not ( ( ( x = board[0] ) and ( x = board[1] ) ) or
+                     ( ( x = board[5] ) and ( x = board[8] ) ) or
+                     ( ( x = board[4] ) and ( x = board[6] ) ) )
+                then x := PieceBlank;
+            end;
+        3:  begin
+            x := board[ 3 ];
+            if not ( ( ( x = board[4] ) and ( x = board[5] ) ) or
+                     ( ( x = board[0] ) and ( x = board[6] ) ) )
+                then x := PieceBlank;
+            end;
+        4:  begin
+            x := board[ 4 ];
+            if not ( ( ( x = board[0] ) and ( x = board[8] ) ) or
+                     ( ( x = board[2] ) and ( x = board[6] ) ) or
+                     ( ( x = board[1] ) and ( x = board[7] ) ) or
+                     ( ( x = board[3] ) and ( x = board[5] ) ) )
+                then x := PieceBlank;
+            end;
+        5:  begin
+            x := board[ 5 ];
+            if not ( ( ( x = board[3] ) and ( x = board[4] ) ) or
+                     ( ( x = board[2] ) and ( x = board[8] ) ) )
+                then x := PieceBlank;
+            end;
+        6:  begin
+            x := board[ 6 ];
+            if not ( ( ( x = board[7] ) and ( x = board[8] ) ) or
+                     ( ( x = board[0] ) and ( x = board[3] ) ) or
+                     ( ( x = board[4] ) and ( x = board[2] ) ) )
+                then x := PieceBlank;
+            end;
+        7:  begin
+            x := board[ 7 ];
+            if not ( ( ( x = board[6] ) and ( x = board[8] ) ) or
+                     ( ( x = board[1] ) and ( x = board[4] ) ) )
+                then x := PieceBlank;
+            end;
+        8:  begin
+            x := board[ 8 ];
+            if not ( ( ( x = board[6] ) and ( x = board[7] ) ) or
+                     ( ( x = board[2] ) and ( x = board[5] ) ) or
+                     ( ( x = board[0] ) and ( x = board[4] ) ) )
+                then x := PieceBlank;
+            end;
+    end;
+
+    winner2 := x;
+end;
+
+function minmax( alpha: integer; beta: integer; depth: integer; move: integer ): integer;
 var
   p, value, pieceMove, score : integer;
 begin
@@ -99,7 +169,7 @@ begin
   value := scoreInvalid;
   if depth >= 4 then
   begin
-    p := lookForWinner;
+    p := winner2( move ); { lookForWinner; }
     if p <> pieceBlank then
     begin
       if p = pieceX then
@@ -129,7 +199,7 @@ begin
       if board[ p ] = pieceBlank then
       begin
         board[ p ] := pieceMove;
-        score := minmax( alpha, beta, depth + 1 );
+        score := minmax( alpha, beta, depth + 1, p );
         board[ p ] := pieceBlank;
 
         if Odd( depth ) then
@@ -203,7 +273,7 @@ var
   score: integer;
 begin
   board[move] := pieceX;
-  score := minmax( scoreMin, scoreMax, 0 );
+  score := minmax( scoreMin, scoreMax, 0, move );
   board[move] := pieceBlank;
 end;
 
